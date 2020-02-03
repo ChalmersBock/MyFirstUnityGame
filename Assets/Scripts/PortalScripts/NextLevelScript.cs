77f0d1c5086b0acd946ef8d2bcc43e75154aaea5
ChalmersBock <erikboc@student.chalmers.se>
2020-02-03 17:37:34 +0100

    Lots of additions
    Added another level and a start screen. Made almost all objects into prefabs that can be reused. Changed some of the srcipts and added new ones. Added the portal that takes the player to the next level

---
+++ Assets/Scripts/PortalScripts/NextLevelScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class NextLevelScript : MonoBehaviour
+{
+    public string sceneName;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/StartGameScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class StartGameScript : MonoBehaviour
+{
+
+    public string sceneName;
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        if (Input.anyKey)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+   
+
+    }
+}
